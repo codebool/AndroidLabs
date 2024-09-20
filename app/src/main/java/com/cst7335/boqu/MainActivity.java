37d701eb01554f1166968a252e6a1fe3b6e7b34b
CodeBool <52304840+codebool@users.noreply.github.com>
2024-09-20 16:28:22 -0400

    step 1

--- app/src/main/java/com/cst7335/boqu/MainActivity.java
+++ app/src/main/java/com/cst7335/boqu/MainActivity.java
@@ -1,12 +1,15 @@
 package com.cst7335.boqu;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.snackbar.Snackbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_main_linear);
+
+        Button clickButton = findViewById(R.id.button);
+        clickButton.setOnClickListener(v -> {
+            String message = getResources().getString(R.string.toast_message);
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+        });
+
+        Switch switchButton = findViewById(R.id.switch_button);
+        switchButton.setOnCheckedChangeListener((cb, isChecked) -> {
+            String switchState = isChecked ? "on" : "off";
+            String info = getResources().getString(R.string.switch_mode);
+            Snackbar snackbar = Snackbar.make(findViewById(R.id.main), info + switchState, Snackbar.LENGTH_LONG);
+            snackbar.setAction("Undo", v -> cb.setChecked(!isChecked));
+            snackbar.show();
         });
     }
 }
